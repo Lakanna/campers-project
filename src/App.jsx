ee96cd1e7ac0ab8efd214a490c542048041f0eec
Hanna Lakus <annalakus@ukr.net>
2025-01-15 00:03:36 +0000

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/campers" element={<div>List of campers page</div>} />
+        <Route
+          path="/campers/:campersId"
+          element={<div>Selected camper page</div>}
+        />
+        <Route path="*" element={<div>Not found page</div>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
