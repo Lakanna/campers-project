858873f8ba26b17f22a65aa5b8f526281cd6e281
Hanna Lakus <annalakus@ukr.net>
2025-01-19 22:17:12 +0000

    changed logo svg

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 
 import Icon from '../Icon/Icon.jsx';
+import css from './Logo.module.css';
 
 export default function Logo() {
   return (
     <Link to="/">
-      <Icon id="Logo" width={136} height={15} className="custom-class" />
+      <Icon id="Logo1" width={63} height={15} className="custom-class" />
+      <Icon id="Logo2" width={71} height={15} className={css.second} />
     </Link>
   );
 }
